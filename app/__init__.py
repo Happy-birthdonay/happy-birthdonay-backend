9e4be70c503bbd6747bf30e0d4bffcd285fc4b00
Eunbin Kwon <kwoneunbini@gmail.com>
2024-03-13 14:48:24 +0900

    feat: added JWT creation code

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from config import DB_URL
+from config import DB_URL, JWT_SECRET_KEY
 
 from flask import Flask
 from flask_cors import CORS
@@ -10,6 +10,7 @@ def create_app(test_config=None):
     app = Flask(__name__)
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+    app.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
 
     # Database Setting
     db.init_app(app)
--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 from flask import request, jsonify, logging
+from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token
 
-from app import create_app
+from app import create_app, db
 
 from app.controllers.kakao_oauth_controller import KakaoOAuthController
 
+from app.models import user_model as user
+
 # Start of the App
 app = create_app()
+jwt = JWTManager(app)
 
 
 # Routes
@@ -28,23 +32,30 @@ def kakao_oauth():
 
     if access_token is None or user_infos is None:
         # TODO: - Make the response more specific
-        return jsonify({
-            'result': 'failure',
-            'message': '카카오 로그인에 실패했습니다.'
-        }), 401
+        return jsonify(result='failure',
+                       message='Failed Kakao Login'), 401
     else:
-        res = jsonify({
-            'result': 'success',
-            'message': '카카오 로그인에 성공했습니다.',
-            'data': {
-                'name': user_infos['kakao_account']['name'],
-                'birthday': user_infos['kakao_account']['birthday'],
-            }
-        })
+        # TODO: - 1. Store user info in the database
+        # TODO: - 2. Get user id from the database
+        # TODO: - 3. Make our own tokens
 
-        # TODO: - Make our own access_token
-        new_access_token = 'temporary_new_access_token'
-        new_refresh_token = 'temporary_new_refresh_token'
+        # new_user = user.User(name=user_infos['kakao_account']['name'],
+        #                      birthday=user_infos['kakao_account']['birthday'])
+        new_user = user.User(name="eunbin",
+                             birthday="0128")
+        # MARK: - How to convert the string to datetime with format(YYYYMMDD)?
+        new_user_id = 222
+
+        # db.session.add(new_user)
+        # db.session.commit()
+        # MARK: - How to get the user id from the database?
+
+        new_access_token = create_access_token(identity=new_user_id, additional_claims=new_user.to_json())
+        new_refresh_token = create_refresh_token(identity=new_user_id, additional_claims=new_user.to_json())
+
+        res = jsonify(result='success',
+                      message='Succeeded Kakao Login',
+                      data=new_user.to_json())
 
         res.set_cookie('access_token', new_access_token)
         res.set_cookie('refresh_token', new_refresh_token)
