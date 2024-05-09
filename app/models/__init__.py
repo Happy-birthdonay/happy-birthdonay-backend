f98583311b349c04d1ed96dca0dc4ff5af8a31ac
Eunbin Kwon <kwoneunbini@gmail.com>
2024-05-09 16:49:22 +0900

    feat: added secret related codes to the model init file

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-from config import DB_URL
+from app.controllers import google_secret_controller
+
+
+def get_secrets(secret_key_str):
+    secret_controller = google_secret_controller.GoogleSecretController()
+    return secret_controller.access_secret(secret_key_str)
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+app.config['SQLALCHEMY_DATABASE_URI'] = get_secrets('db_url')
 db = SQLAlchemy(app)
-
